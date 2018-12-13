51d85b44c9aa5c213c8b35059c47758b83db4bae
Jiexin Guo <jiexing.jg@gmail.com>
2018-12-12 23:47:27 -0500

    An editable text field at the appBar.

--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -1,2 +1,52 @@
 import 'package:flutter/material.dart';
 
+class SearchBar extends StatefulWidget {
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  // Create a text controller. We will use it to retrieve the current value
+  // of the TextField!
+  static final String startSearchValue = '🔎 Click to start search';
+  final myController = TextEditingController(text: startSearchValue);
+  bool startTyping = false;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    controller: myController,
+                    onTap: () {
+                      if (!startTyping) {
+                        myController.clear();
+                        startTyping = true;
+                      }
+                    },
+                    onSubmitted: (text) {
+                      if (text.isEmpty) {
+                        startTyping = false;
+                        myController.text = startSearchValue;
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
