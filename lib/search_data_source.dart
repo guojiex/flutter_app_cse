be4445b154c8932087e506a15392e49d67e6d6ad
Jiexin Guo <jiexing.jg@gmail.com>
2018-12-14 11:11:18 -0500

    Add some basic search data source implement. Step 1.

--- lib/search_data_source.dart
+++ lib/search_data_source.dart
@@ -1,8 +1,35 @@
 import 'package:meta/meta.dart';
 
-class SearchResult {}
+// TODO: Use https://pub.dartlang.org/packages/url_launcher to let SearchResult capable to open apps.
+@immutable
+class SearchResult {
+  final String title;
+  final String link;
+  final String snippet;
 
-abstract class SearchDataSource<SearchResult> {}
+  SearchResult(this.title, this.link, this.snippet);
+}
+
+abstract class SearchDataSource {
+  List<SearchResult> search(String query);
+}
+
+class FakeSearchDataSource implements SearchDataSource {
+  @override
+  List<SearchResult> search(String query) {
+    return [new SearchResult('title', 'www.google.com', 'A fake test example')]
+        .toList();
+  }
+}
+
+class CustomSearchJsonDataSource implements SearchDataSource {
+  final String cx;
+
+  CustomSearchJsonDataSource(this.cx);
+
+  @override
+  List<SearchResult> search(String query) {}
+}
 
 abstract class AutoCompleteDataSource {
   List<String> getAutoCompletions({String query, int resultNumber});
---
+++ lib/search_result_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_cse/search_data_source.dart';
+
+class SearchResultPage extends StatelessWidget {
+  final List<SearchResult> _searchResults;
+
+  SearchResultPage(this._searchResults);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column();
+  }
+}
