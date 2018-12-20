ffa4c75529d2561fd9b72e814908ef2e464fbc3f
Jiexin Guo <jiexing.jg@gmail.com>
2018-12-20 15:13:35 -0500

    Add to try google custom search api in test.

--- test/search_data_source_test.dart
+++ test/search_data_source_test.dart
@@ -1,4 +1,5 @@
 import 'package:test/test.dart';
+import 'dart:async';
 import 'package:flutter_app_cse/search_data_source.dart';
 
 void main() {
@@ -6,5 +7,11 @@ void main() {
     var data_source = FakeAutoCompleteDataSource();
     expect(data_source.getAutoCompletions(query: ''),
         ['abcd', 'efgh', 'efdfsjd'].toList());
+  });
+  test('CSE data source test', () {
+    final apiKey = '<please-fill-in>';
+    var data_source = CustomSearchJsonDataSource(
+        cx: '008795855128244970711:yafufe8jjt8', apiKey: apiKey);
+    data_source.search('test');
   });
 }
